dedc6a97f14e9c37d051c7c35358a6abfed888ce
Dean Simon R. Damasig <ddamasig@gmail.com>
2022-04-24 00:51:00 +0800

    Fixed password setting in CreateSuperAdmin command.

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle an authentication attempt.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function authenticate(Request $request)
+    {
+        $request->validate([
+            'username' => 'required',
+            'password' => 'required',
+        ]);
+
+        $login_type = filter_var($request->input('username'), FILTER_VALIDATE_EMAIL)
+            ? 'email'
+            : 'mobile_number';
+
+        $request->merge([
+            $login_type => $request->input('username')
+        ]);
+
+        if (Auth::attempt($request->only($login_type, 'password'))) {
+            $request->session()->regenerate();
+
+            return response()->json([
+                'message' => 'Successfully authenticated.'
+            ], 200);
+        }
+
+        return response()->json([
+            'message' => 'The provided credentials do not match our records.',
+        ], 403);
+    }
+
+
+    /**
+     * Log the user out of the application.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return response()->json([
+            'message' => 'Successfully logged out.'
+        ], 200);
+    }
+}
